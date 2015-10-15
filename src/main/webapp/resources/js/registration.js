9795056a5b39f85aed6f9ae5ff4a71a457622f42
cpuzzuol <cpuzzuol@public-45-112.emich.edu>
2015-10-15 17:32:26 -0400

    last good working configuration before trying to include angular-resource from bower

--- src/main/webapp/resources/js/registration.js
+++
@@ -1,5 +0,0 @@
-var registerApp = angular.module('registrationApp',[]);
-
-registerApp.controller('RegistrationController', ['$scope', function($scope) {
-    $scope.reg = "Register variable!";
-}]);
