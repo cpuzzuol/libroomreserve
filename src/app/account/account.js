b3c2aa65f200b9a1f905abf0e238a37fa3d3a1c5
cpuzzuol <cpuzzuol@public-45-112.emich.edu>
2015-10-16 18:00:11 -0400

    ngResource works...user records partially saving to database...end of day 10/16

---
+++ src/app/account/account.js
@@ -0,0 +1,72 @@
+angular.module('ngBoilerplate.account', ['ui.router', 'ngResource'])
+
+.config(function($stateProvider){
+    $stateProvider
+        .state('login', {
+           url: '/login',
+           views: {
+               'main': {
+                   templateUrl: 'account/login.tpl.html',
+                   controller: 'LoginCtrl'
+               }
+           },
+           data: { pageTitle: 'Login' }
+        })
+        .state('register', {
+          url: '/register',
+          views: {
+              'main': {
+                  templateUrl: 'account/register.tpl.html',
+                  controller: 'RegisterCtrl'
+              }
+          },
+          data: { pageTitle: 'Register' }
+        });
+})
+
+//switch $state if user logged in
+.controller("LoginCtrl", function($scope, sessionService, $state){
+    $scope.login = function() {
+        sessionService.login($scope.account);
+        $state.go("home");
+    };
+})
+.controller("RegisterCtrl", function($scope, sessionService, $state, accountService){
+    $scope.register = function(){
+        accountService.register($scope.account,
+            function(returnedData){
+                sessionService.login($scope.account);
+                $state.go("home");
+            },
+            function(){
+                console.log("Error registering user!");
+            });
+    };
+})
+//a factory service is similar to a java @Bean
+//returns a JSON object representing the session service
+.factory('sessionService', function(){
+    var session = {};
+    session.login = function(data){
+        localStorage.setItem("session", data);
+        console.log("Logged in user: " + data.name + " / " + data.password);
+    };
+    session.logout = function(){
+        localStorage.removeItem("session");
+        console.log("User has been logged out.");
+    };
+    session.isLoggedIn = function(){
+        return localStorage.getItem("session") !== null;
+    };
+    return session;
+})
+
+.factory('accountService', function($resource){
+    var service = {};
+    service.register = function(account, success, failure){
+        var Account = $resource("/libroomreserve/api/user");
+        Account.save({}, account, success, failure);
+    };
+    return service;
+});
+
