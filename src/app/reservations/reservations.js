a32c91715f3d40a30ac76351ccc3dca0c6fe6b5f
cpuzzuol <cpuzzuol@164.76.47.224>
2015-11-06 16:18:59 -0500

    trying to get $http.get() data to bind to variables (something about $q.defer()...end of day 11/6

--- src/app/reservations/reservations.js
+++ src/app/reservations/reservations.js
@@ -1,4 +1,4 @@
-angular.module('ngBoilerplate.reservations',['ui.router', 'ngResource'])
+angular.module('ngBoilerplate.reservations',['ui.router', 'ui.bootstrap', 'ngResource'])
 
 .config(function($stateProvider){
     $stateProvider
@@ -13,8 +13,110 @@ angular.module('ngBoilerplate.reservations',['ui.router', 'ngResource'])
             data: { pageTitle: 'New Reservation' }
         });
 })
-.controller('AddReservationController', function($scope, $http){
-    $scope.hello = "Let's reserve a new room, shall we?";
+
+.factory('reservationService', function($resource, $q, $http){
+    var reservations = {};
+    
+    reservations.addReservation = function(reservation, success, failure){
+        console.log("DOGGIE AFUERA!");
+        console.log(reservation.room);
+        
+        var packagedUser = $http.get("/libroomreserve/api/room/" + reservation.room).then(function(response){
+                console.log("success http!");
+                console.log(response.data);
+                return response.data;
+        });
+        
+        /*
+        function(){
+                    return this.fetchRoomObj(
+                            reservation.room, 
+                            function(data){
+                                console.log("fetch room ran!");
+                                console.log(data);
+                                return data;
+                            }, 
+                            function(){console.log("fetch room FAILED!");}
+                        );*/
+        
+        var preparedReservation = {
+            room: packagedUser,
+            user: reservation.user,
+            startTime: reservation.startTime,
+            endTime: reservation.endTime,
+            note: reservation.note
+        };
+        console.log(preparedReservation);
+        var Reservation = $resource('/libroomreserve/api/reservation');
+        Reservation.save({}, preparedReservation, success, failure);
+    };
+    
+    reservations.listRooms = function(success, failure){
+        var RoomsList = $resource('/libroomreserve/api/room');
+        RoomsList.get({}, success, failure);
+    };
+    
+    reservations.fetchRoomObj = function(room){
+        //var Room = $resource('/libroomreserve/api/room/67');
+        //Room.get({roomId: room.roomId}, success, failure);
+        $http.get("/libroomreserve/api/room/" + room).then(function(response){
+                console.log("success http!");
+                console.log(response.data);
+                finalRoom = response;
+        });
+    };
+    
+    return reservations;
+})
+.controller('AddReservationController', function($scope, $state, $http, reservationService){
+        $scope.reservation = {};
+        
+    //get all rooms
+        reservationService.listRooms(
+            function(data){
+                $scope.roomsList = data.roomResources;
+            },
+            function(){
+                $scope.roomsList = null;
+            }
+        );  
+            
+        //fetch user object of logged in user
+        $http.get("/libroomreserve/api/user/1").then(
+            function(resource){
+                console.log(resource);
+                $scope.reservation.user = resource.data;
+            },
+            function(){
+                $scope.reservation.user = null;
+            }
+        );
+        $scope.newReservation = function(){
+            //fetch room object based on submitted roomId
+            /*
+            $http.get("/libroomreserve/api/room/" + $scope.reservation.room).then(
+                function(resource){
+                    console.log("FRENCH PIANO!");
+                    console.log(resource.data);
+                    $scope.reservation.room = resource.data;
+                },
+                function(){
+                    $scope.reservation.room = null;
+                }
+            );
+            */
+            
+            reservationService.addReservation(
+                $scope.reservation,
+                function(data){
+                    console.log("Success! Data printing:");
+                    $state.go("home");
+                },
+                function(data){
+                    console.log("Failure! Data printing:");
+                }
+            );
+        };
 });
 
 
