a22a84c582f8ff84292de2569193973496c34a13
cpuzzuol <cpuzzuol@public-43-93.emich.edu>
2015-11-09 12:39:25 -0500

    completed room and user object promise (reservation.js)

--- src/app/reservations/reservations.js
+++ src/app/reservations/reservations.js
@@ -18,9 +18,6 @@ angular.module('ngBoilerplate.reservations',['ui.router', 'ui.bootstrap', 'ngRes
     var reservations = {};
     
     reservations.addReservation = function(reservation, success, failure){
-        console.log("DOGGIE AFUERA!");
-        console.log(reservation.room);
-        
         var Reservation = $resource('/libroomreserve/api/reservation');
         Reservation.save({}, reservation, success, failure);
     };
@@ -30,12 +27,50 @@ angular.module('ngBoilerplate.reservations',['ui.router', 'ui.bootstrap', 'ngRes
         RoomsList.get({}, success, failure);
     };
     
+    //This function has been replaced by fetchRoomUserObjs (LEAVE FOR REFERENCE)
+    /*
     reservations.fetchRoomObj = function(roomId){
-        //var Room = $resource('/libroomreserve/api/room/67');
-        //Room.get({roomId: room.roomId}, success, failure);
-        return $http.get("/libroomreserve/api/room/" + roomId).then(function(response){
-                return response;
-        });
+        return $http.get("/libroomreserve/api/room/" + roomId).then(
+            function(response){
+                return response.data;
+            },
+            function(httpError){
+                //translate the error
+                throw httpError.status + " : " +
+                        httpError.data;
+            }
+        );
+    }; */
+    
+    //uses $q to process multiple asynchronous calls
+    reservations.fetchRoomUserObjs = function(roomId, userId){
+        var deferred = $q.defer();
+        var urlCalls = [];
+        
+        //call to room API
+        urlCalls.push($http.get("/libroomreserve/api/room/" + roomId));
+        //call to user API
+        urlCalls.push($http.get("/libroomreserve/api/user/" + userId));
+        
+        //execute callbacks once they are completely finished
+        $q.all(urlCalls).then(
+            //success
+            function(results){
+                console.log("SUCCESS Q-ALL. Results: ");
+                deferred.resolve(results);
+            },
+            //error
+            function(errors){
+                console.log("REJECTED Q-ALL. Errors: ");
+                deferred.reject(errors);
+            },
+            //update
+            function(updates){
+                console.log("RESPONSE Q-ALL. Updates: ");
+                deferred.update(updates);
+            }
+        );
+        return deferred.promise;
     };
     
     return reservations;
@@ -44,7 +79,7 @@ angular.module('ngBoilerplate.reservations',['ui.router', 'ui.bootstrap', 'ngRes
 .controller('AddReservationController', function($scope, $state, $http, reservationService){
         $scope.reservation = {};
         
-    //get all rooms
+        //get all rooms
         reservationService.listRooms(
             function(data){
                 $scope.roomsList = data.roomResources;
@@ -65,26 +100,26 @@ angular.module('ngBoilerplate.reservations',['ui.router', 'ui.bootstrap', 'ngRes
             }
         );
         $scope.newReservation = function(){
-            reservationService.fetchRoomObj($scope.reservation.room).then(function(roomObj){
-                var finalReservation = {
-                    room: roomObj.data,
-                    user: $scope.reservation.user,
-                    startTime: $scope.reservation.startTime,
-                    endTime: $scope.reservation.endTime,
-                    note: $scope.reservation.note
-                };
-                reservationService.addReservation(
-                    finalReservation,
-                    function(data){
-                        console.log("Success! Data printing:");
-                        $state.go("home");
-                    },
-                    function(data){
-                        console.log("Failure! Data printing:");
-                    }
-                );
-            });
-            
+            reservationService.fetchRoomUserObjs($scope.reservation.room, 1).then(
+                function(roomAndUserObjs){
+                    var finalReservation = {
+                        room: roomAndUserObjs[0].data, //room object returned in promise
+                        user: roomAndUserObjs[1].data, //user object returned in promise
+                        startTime: $scope.reservation.startTime,
+                        endTime: $scope.reservation.endTime,
+                        note: $scope.reservation.note
+                    };
+                    reservationService.addReservation(
+                        finalReservation,
+                        function(data){
+                            $state.go("home");
+                        },
+                        function(data){
+                            $scope.error = true;
+                        }
+                    );
+                }
+            );
         };
 });
 
